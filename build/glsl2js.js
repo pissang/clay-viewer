a6ded6f6f429847b46edb8950aa635fdcb375f2e
pissang <bm2736892@gmail.com>
2018-03-01 14:45:20 +0800

    Release 0.2.0

--- build/glsl2js.js
+++ build/glsl2js.js
@@ -3,16 +3,16 @@ var fs = require('fs');
 
 glob(__dirname + '/../{src,editor}/**/*.glsl', function (err, files) {
     files.forEach(function (filePath) {
-        var esslCode = fs.readFileSync(filePath, 'utf-8');
+        var glslCode = fs.readFileSync(filePath, 'utf-8');
         // TODO Remove comment
-        esslCode = esslCode.replace(/\/\/.*\n/g, '');
-        esslCode = esslCode.replace(/ +/g, ' ');
+        glslCode = glslCode.replace(/\/\/.*\n/g, '');
+        glslCode = glslCode.replace(/ +/g, ' ');
 
         // var dir = path.dirname(filePath);
         // var baseName = path.basename(filePath, '.essl');
         fs.writeFileSync(
             filePath + '.js',
-               'export default ' + JSON.stringify(esslCode) + ';\n',
+               'export default ' + JSON.stringify(glslCode) + ';\n',
             'utf-8'
         );
     });
