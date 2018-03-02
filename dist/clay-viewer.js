7b5f77497b4e41b7afbf7ff665dcf9999b995d54
pissang <bm2736892@gmail.com>
2018-03-02 14:28:23 +0800

    Rebuild 0.2.1

--- dist/clay-viewer.js
+++ dist/clay-viewer.js
@@ -11619,7 +11619,7 @@ var Renderer = Base.extend(function () {
             return;
         }
         camera.update();
-        var renderList = scene.updateRenderList(camera);
+        var renderList = scene.updateRenderList(camera, true);
 
         this._sceneRendering = scene;
 
@@ -14974,9 +14974,10 @@ var Scene = Node.extend(function () {
      * It needs camera for the frustum culling.
      *
      * @param {clay.Camera} camera
+     * @param {boolean} updateSceneBoundingBox
      * @return {clay.Scene.RenderList}
      */
-    updateRenderList: function (camera) {
+    updateRenderList: function (camera, updateSceneBoundingBox) {
         var id = camera.__uid__;
         var renderList = this._renderLists.get(id);
         if (!renderList) {
@@ -14985,11 +14986,13 @@ var Scene = Node.extend(function () {
         }
         renderList.startCount();
 
-        this.viewBoundingBoxLastFrame.min.set(Infinity, Infinity, Infinity);
-        this.viewBoundingBoxLastFrame.max.set(-Infinity, -Infinity, -Infinity);
+        if (updateSceneBoundingBox) {
+            this.viewBoundingBoxLastFrame.min.set(Infinity, Infinity, Infinity);
+            this.viewBoundingBoxLastFrame.max.set(-Infinity, -Infinity, -Infinity);
+        }
 
         var sceneMaterialTransparent = this.material && this.material.transparent || false;
-        this._doUpdateRenderList(this, camera, sceneMaterialTransparent, renderList);
+        this._doUpdateRenderList(this, camera, sceneMaterialTransparent, renderList, updateSceneBoundingBox);
 
         renderList.endCount();
 
@@ -15005,7 +15008,7 @@ var Scene = Node.extend(function () {
         return this._renderLists.get(camera.__uid__);
     },
 
-    _doUpdateRenderList: function (parent, camera, sceneMaterialTransparent, renderList) {
+    _doUpdateRenderList: function (parent, camera, sceneMaterialTransparent, renderList, updateSceneBoundingBox) {
         if (parent.invisible) {
             return;
         }
@@ -15019,12 +15022,12 @@ var Scene = Node.extend(function () {
                 var geometry = child.geometry;
 
                 mat4$3.multiplyAffine(WORLDVIEW, camera.viewMatrix.array, worldM);
-                if (!geometry.boundingBox || !this.isFrustumCulled(child, camera, WORLDVIEW)) {
+                if (updateSceneBoundingBox && !geometry.boundingBox || !this.isFrustumCulled(child, camera, WORLDVIEW)) {
                     renderList.add(child, child.material.transparent || sceneMaterialTransparent);
                 }
             }
             if (child._children.length > 0) {
-                this._doUpdateRenderList(child, camera, sceneMaterialTransparent, renderList);
+                this._doUpdateRenderList(child, camera, sceneMaterialTransparent, renderList, updateSceneBoundingBox);
             }
         }
     },
@@ -27224,7 +27227,7 @@ var GBuffer = Base.extend(function () {
         var frameBuffer = this._frameBuffer;
         var viewport = frameBuffer.viewport;
 
-        var renderList = scene.updateRenderList(camera);
+        var renderList = scene.updateRenderList(camera, true);
 
         var opaqueList = renderList.opaque;
         var transparentList = renderList.transparent;
